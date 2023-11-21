f4d8058a4ae4f222097fbb926b35d7ab7db80ebe
Anu Olajide <102215210+pasta022@users.noreply.github.com>
2023-11-21 11:57:10 +0100

    readme file added

---
+++ README.md
@@ -0,0 +1,25 @@
+# Tic Tac Toe
+
+A simple two player tic tac toe game made using html, css and vanilla javascript.
+
+## Features
+
+It is played like a regular tic tac toe game where players try to get their letters in a pattern of three to win. the design is basic but the aim of the code is in the logic; to display an understanding of javascript.
+
+## Getting Started
+
+the game begins with player x. simply start anywhere on the board to start playing
+
+## Limitations
+
+no limitations on the app
+
+## Contributing
+
+Contributions are welcome! If you have any suggestions or improvements, feel free to open an issue or submit a pull request.
+
+## Contact
+
+for questions or inquries, please contact oluwamayowa00@gmail.com
+
+Thank You!
--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 // Define Values
-const boxElements = document.querySelectorAll('.box');
-const gameELement = document.querySelector('#board');
-const announcer = document.querySelector('.winning-text');
-const announcer2 = document.querySelector('.announce-text');
-const winSpecifier = document.querySelector('.specifier');
-const restartButton = document.getElementById('reset');
-let X_CLASS = 'cross';
-let O_CLASS = 'circle';
+const boxElements = document.querySelectorAll(".box");
+const gameELement = document.querySelector("#board");
+const announcer = document.querySelector(".winning-text");
+const announcer2 = document.querySelector(".announce-text");
+const winSpecifier = document.querySelector(".specifier");
+const restartButton = document.getElementById("reset");
+const announceHeader = document.querySelector(`.announce-header`);
+const headerSpecifier = document.querySelector(`.header-specifier`);
+
+let X_CLASS = "cross";
+let O_CLASS = "circle";
 let currentPlayer = X_CLASS;
 /* board index
     [0] [1] [2]
@@ -14,109 +17,106 @@ let currentPlayer = X_CLASS;
     [6] [7] [8]
 */
 let winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 //Start Game
 startGame();
 
-
 function startGame() {
-    announcer.classList.add('hide');
-    boxElements.forEach(box => {
-        box.classList.remove(X_CLASS);
-        box.classList.remove(O_CLASS);
-        gameELement.classList.remove(X_CLASS);
-        gameELement.classList.remove(O_CLASS);
-        gameELement.classList.add(currentPlayer);
-        box.innerHTML = '';
-        box.addEventListener('click', click, { once: true });
-    })
+  announcer.classList.add("hide");
+  boxElements.forEach((box) => {
+    box.classList.remove(X_CLASS);
+    box.classList.remove(O_CLASS);
+    gameELement.classList.remove(X_CLASS);
+    gameELement.classList.remove(O_CLASS);
+    gameELement.classList.add(currentPlayer);
+    box.innerHTML = "";
+    box.addEventListener("click", click, { once: true });
+  });
 }
 
-
-//Click function 
+//Click function
 function click(event) {
-    const box = event.target;
-    placemark(box, currentPlayer);
-    placePlayer(box, currentPlayer);
-    checkWin(currentPlayer);
-    currentPlayer = currentPlayer === X_CLASS ? O_CLASS : X_CLASS;
+  const box = event.target;
+  placemark(box, currentPlayer);
+  placePlayer(box, currentPlayer);
+  checkWin(currentPlayer);
+  currentPlayer = currentPlayer === X_CLASS ? O_CLASS : X_CLASS;
+  headerSpecifier.innerHTML = currentPlayer === X_CLASS ? "X" : "O";
 }
 
 //add class
 const placemark = (box, currentPlayer) => {
-    box.classList.add(currentPlayer);
-}
+  box.classList.add(currentPlayer);
+};
 
 //add current player
 const placePlayer = (box, currentPlayer) => {
-    if (currentPlayer === X_CLASS) {
-        box.innerHTML = 'X';
-    }
-    else {
-        box.innerHTML = 'O';
-    }
-}
+  if (currentPlayer === X_CLASS) {
+    box.innerHTML = "X";
+  } else {
+    box.innerHTML = "O";
+  }
+};
 
 //change Turn
-const playerTurn =(currentPlayer) => {
-    gameELement.classList.remove(X_CLASS);
-    gameELement.classList.remove(O_CLASS);
-    if (currentPlayer === X_CLASS){
-        gameELement.classList.add(X_CLASS);
-    } else {
-        gameELement.classList.add(O_CLASS);
-    }
-}
+const playerTurn = (currentPlayer) => {
+  gameELement.classList.remove(X_CLASS);
+  gameELement.classList.remove(O_CLASS);
+  if (currentPlayer === X_CLASS) {
+    gameELement.classList.add(X_CLASS);
+  } else {
+    gameELement.classList.add(O_CLASS);
+  }
+};
 
 // Check for Win
 const checkWin = (currentPlayer) => {
-    if (gameWon(currentPlayer)) {
-        endgame(false);
-    }
-    else if (isDraw()) {
-        endgame(true)
-    } 
-    else {
-        currentPlayer = currentPlayer === X_CLASS ? O_CLASS : X_CLASS;
-        playerTurn(currentPlayer);
-        console.log(currentPlayer);
-    }
-}
+  if (gameWon(currentPlayer)) {
+    endgame(false);
+  } else if (isDraw()) {
+    endgame(true);
+  } else {
+    currentPlayer = currentPlayer === X_CLASS ? O_CLASS : X_CLASS;
+    playerTurn(currentPlayer);
+    console.log(currentPlayer);
+  }
+};
 function gameWon(currentPlayer) {
-    return winningConditions.some(combination => {
-        return combination.every(index => {
-            return boxElements[index].classList.contains(currentPlayer);
-        });
+  return winningConditions.some((combination) => {
+    return combination.every((index) => {
+      return boxElements[index].classList.contains(currentPlayer);
     });
+  });
 }
 function endgame(draw) {
-    if (draw){
-        announcer2.innerHTML = 'Draw!'
-        announcer.classList.remove('hide');
-    }
-    else {
-        currentPlayer === X_CLASS ? announcer2.innerHTML = 'Player X wins' : announcer2.innerHTML = 'Player O Wins';
-        announcer.classList.remove('hide');
-    }
+  if (draw) {
+    announcer2.innerHTML = "Draw!";
+    announcer.classList.remove("hide");
+  } else {
+    currentPlayer === X_CLASS
+      ? (announcer2.innerHTML = "Player X wins")
+      : (announcer2.innerHTML = "Player O Wins");
+    announcer.classList.remove("hide");
+  }
 }
 function isDraw() {
-    return[...boxElements].every(box => {
-        return box.classList.contains(X_CLASS)|| box.classList.contains(O_CLASS);
-    })
+  return [...boxElements].every((box) => {
+    return box.classList.contains(X_CLASS) || box.classList.contains(O_CLASS);
+  });
 }
 
 //New Game Button
-restartButton.addEventListener('click', startGame);
+restartButton.addEventListener("click", startGame);
 
 //reload function
 function reload() {
-    window.location.reload();
+  window.location.reload();
 }
